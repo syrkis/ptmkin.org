f097f3a0c3c4287a533929f516fe4f0966d70283
Noah Syrkis <noah@syrkis.com>
2023-05-28 13:43:38 +0200

    netlify adapter

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte'
 
--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    prerender: {
+      entries: [
+        '/:slug' // Include the dynamic route for blog posts
+      ]
+    },
         adapter: adapter({
             // if true, will create a Netlify Edge Function rather
             // than using standard Node-based functions
