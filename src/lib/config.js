a99398cf0190247b99b993f3ae729bc538d463ae
Noah Syrkis <noah@syrkis.com>
2023-05-28 12:56:40 +0200

    netlify adapter

--- src/lib/config.js
+++
@@ -1,8 +0,0 @@
-export const siteTitle = 'Potemkin Contemporary'
-export const siteDescription = 'Potemkin Contemporary'
-export const siteURL = 'ptmkin.org' //TODO
-export const siteLink = 'https://github.com/syrkis/ptmkin.org'
-export const siteAuthor = 'Noah Syrkis'
-
-// Controls how many posts are shown per page on the main blog index pages
-export const postsPerPage = 9999
