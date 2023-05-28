75bcb2e261068db71961f927b86dc31ebbe62783
Noah Syrkis <noah@syrkis.com>
2023-05-28 18:14:08 +0200

    move posts to static

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -6,7 +6,7 @@ import type { Post } from '$lib/types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	try {
-		const post = fm<Post>(fs.readFileSync(`static/posts/${params.slug}.md`, 'utf-8'));
+		const post = fm<Post>(fs.readFileSync(`%sveltekit.assets%/posts/${params.slug}.md`, 'utf-8'));
 
 		console.log(`[info] processing markdown static/posts/${params.slug}.md`);
 
