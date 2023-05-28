7f7ab0b5862313c24b742eeacb1562234a690fd9
Noah Syrkis <noah@syrkis.com>
2023-05-28 19:00:35 +0200

    .gitignore

--- .netlify/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.6df1be9d.js","_app/immutable/chunks/index.838feb98.js"];
-export const stylesheets = ["_app/immutable/assets/0.a1f3d880.css"];
-export const fonts = [];
