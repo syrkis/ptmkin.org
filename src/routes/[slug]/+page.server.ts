9541bd5aedcbf1ff4ef3fb3a76572b452eb5cbf3
Noah Syrkis <noah@syrkis.com>
2023-05-28 19:05:58 +0200

    move posts to static

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,24 +1,16 @@
 import type { PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
 import fs from 'fs';
 import fm from 'front-matter';
 import type { Post } from '$lib/types';
 
 
 export const load: PageServerLoad = async ({ params }) => {
-	try {
 		const post = fm<Post>(fs.readFileSync(`src/lib/posts/${params.slug}.md`, 'utf-8'));
 
 		console.log(`[info] processing markdown src/lib/posts/${params.slug}.md`);
 
-		if (post) {
 			return {
 				title: post.attributes.title,
 				body: post.body,
 			};
-		}
-		throw error(500, 'something wrong with the markdown file');
-	} catch {
-		throw error(404, 'not found');
-	}
 };
