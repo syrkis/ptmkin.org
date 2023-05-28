b2d19266ebbd3d9f09a8a1cf0f18b54a0c75b802
Noah Syrkis <noah@syrkis.com>
2023-05-28 19:20:35 +0200

    move posts to static

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,27 @@
 import type { PageServerLoad } from "./$types";
-import { error } from "@sveltejs/kit";
 import fs from "fs";
 import fm from "front-matter";
+import path from "path";
 import type { Post } from "$lib/types";
 
+const postsDir = path.join(process.cwd(), "src", "lib", "posts");
+
 export const load: PageServerLoad = async () => {
-        const posts = fs.readdirSync("src/lib/posts").map((file) => {
-            const post = fm<Post>(fs.readFileSync(`src/lib/posts/${file}`, "utf-8"));
-            return {
-                slug: file.replace(".md", ""),
-                title: post.attributes.title,
-                body: post.body,
-                date: post.attributes.date,
-            };
-        })
-        return {
-            body: posts,
-        };
+  const posts = fs
+    .readdirSync(postsDir)
+    .map((file) => {
+      const post = fm<Post>(
+        fs.readFileSync(path.join(postsDir, file), "utf-8")
+      );
+      return {
+        slug: file.replace(".md", ""),
+        title: post.attributes.title,
+        body: post.body,
+        date: post.attributes.date,
+      };
+    })
+
+  return {
+    body: posts,
+  };
 };
--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,16 +1,18 @@
 import type { PageServerLoad } from './$types';
 import fs from 'fs';
 import fm from 'front-matter';
+import path from 'path';
 import type { Post } from '$lib/types';
 
+const postsDir = path.join(process.cwd(), 'src', 'lib', 'posts');
 
 export const load: PageServerLoad = async ({ params }) => {
-		const post = fm<Post>(fs.readFileSync(`src/lib/posts/${params.slug}.md`, 'utf-8'));
+  const post = fm<Post>(fs.readFileSync(path.join(postsDir, `${params.slug}.md`), 'utf-8'));
 
-		console.log(`[info] processing markdown src/lib/posts/${params.slug}.md`);
+  console.log(`[info] processing markdown ${params.slug}.md`);
 
-			return {
-				title: post.attributes.title,
-				body: post.body,
-			};
+  return {
+    title: post.attributes.title,
+    body: post.body,
+  };
 };
